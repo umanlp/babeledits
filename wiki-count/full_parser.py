f5190da8aeaa3b81d16ea762a7478536415e5b00
felixgaschi <felix@posos.fr>
2024-07-27 13:09:05 +0000

    make full_parser more memory efficient

--- wiki-count/full_parser.py
+++ wiki-count/full_parser.py
@@ -10,7 +10,7 @@ from os import listdir
 from os import listdir
 from os.path import isfile, join
 import gc
-from collections import defaultdict
+from collections import Counter, defaultdict
 import gzip
 import logging
 import multiprocessing
@@ -35,28 +35,27 @@ def load_names_df(path):
     return df 
 
 
-def parse(path_old, data_queue: Queue):
+def parse(path_old, data):
     """ Reads file, eliminates unneeded data, filters
     """
 
     try:
-        print(f"reading {path_old}")
         with gzip.open(path_old, "rb") as f:
             line = f.readline().decode().strip()
             while line:
                 parts = line.split(" ")
                 if len(parts) < 4:
-                    print(f"Could not parse following line: {line}")
                     continue
                 domain, *title, views, _ = parts
                 title = " ".join(title)
-                data_queue.put((domain, title, int(views)))
+                data[(domain, title)] += int(views)
                 line = f.readline().decode().strip()
     except Exception as e:
-        logging.info(f"SKIP {path_old}", exc_info=e)
+        logging.error(f"SKIP {path_old}", exc_info=e)
 
-
-def threader(q: Queue, data_queue: Queue):
+def threader(q: Queue, data_queue: Queue, soft_limit: int):
+    data = Counter()
+    accumulations = 0
     while True:
         # gets a worker from the queue
         worker = q.get()
@@ -66,39 +65,57 @@ def threader(q: Queue, data_queue: Queue):
         # Run the example job with the avail worker in queue (thread)
         start = time.time()
 
-        parse(worker, data_queue) 
-
+        parse(worker, data)
         delay = time.time() - start
-        print(f"Took {delay:.2f} for {worker}")
+
+        if data_queue.qsize() < soft_limit:
+            print(f"Last file: {delay:.2f}s. Queue is free (size={data_queue.qsize()}), sending data for {accumulations + 1} files")
+            start = time.time()
+            data_queue.put(data)
+            delay = time.time() - start
+            print(f"Delay sending data: {delay:.2f}s")
+            data = Counter()
+            accumulations = 0
+        else:
+            accumulations += 1
+            print(f"Last file: {delay:.2f}s. Queue has reached soft limit ((size={data_queue.qsize()})), will send data later for {accumulations} accumulated files")
+
+    while accumulations > 0 and data_queue.qsize() < soft_limit:
+        time.sleep(0.5)
+    data_queue.put(data)
+    
     data_queue.put(None)
+
 
 def depile_thread(num_threads, save_dir, data_queue: Queue):
     none_count = 0
-    data = defaultdict(lambda: defaultdict(int))
+    data = Counter()
     while none_count < num_threads:
         res = data_queue.get()
         if res is None:
             none_count += 1
         else:
-            data[res[0]][res[1]] += res[2]
+            #data[res[0]][res[1]] += res[2]
+            data.update(res)
     
     print(f"Writing data in {save_dir}")
+    new_data = defaultdict(lambda: defaultdict(int))
+    for (domain, title), views in data.items():
+        new_data[domain][title] = views
     for domain in data:
         with open(os.path.join(save_dir, f"{domain}.csv"), "w") as f:
-            title_with_count = [(title, count) for title, count in data[domain].items()]
+            title_with_count = [(title, count) for title, count in new_data[domain].items()]
             title_with_count.sort(key=lambda x: x[1], reverse=True)
             for title, count in title_with_count:
                 f.write(f"{title} {count}\n")
 
-    data_queue.task_done()
 
 
-
-def start_threads(num_threads, save_dir, q: Queue, data_queue: Queue):
+def start_threads(num_threads, save_dir, q: Queue, data_queue: Queue, soft_limit: int):
     threads = []
     for x in range(num_threads):
         time.sleep(0.05)
-        t = multiprocessing.Process(target=threader, args=(q, data_queue))
+        t = multiprocessing.Process(target=threader, args=(q, data_queue, soft_limit))
 
          # classifying as a daemon, so they will die when the main dies
         t.daemon = True
@@ -121,6 +138,7 @@ def main():
     files_dir = sys.argv[1]
     save_dir = sys.argv[2]
     num_threads = int(sys.argv[3])
+    soft_limit = 2 * num_threads if len(sys.argv) < 5 else int(sys.argv[4])
 
     
 
@@ -134,7 +152,7 @@ def main():
         q.put(worker)
     q.put(None)
 
-    threads = start_threads(num_threads, save_dir, q, data_queue)
+    threads = start_threads(num_threads, save_dir, q, data_queue, soft_limit)
 
     for t in threads:
         t.join()
