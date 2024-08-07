fd6366f1f8f50e30a2a88a2d60f9bdb8152a0a74
Félix Gaschi <felix@posos.fr>
2024-08-07 14:04:48 +0000

    enable multiple methods in multi_hard_edit

--- multi_hard_edit.sh
+++ multi_hard_edit.sh
@@ -3,18 +3,24 @@
 
 # Check if any languages are passed
 if [ "$#" -eq 0 ]; then
-    echo "Usage: $0 lang1 lang2 lang3 ..."
+    echo "Usage: $0 method lang1 lang2 lang3 ..."
+    exit 1
+fi
+if [ "$#" -eq 1 ]; then
+    echo "Usage: $0 method lang1 lang2 lang3 ..."
     exit 1
 fi
 
+method=$1
+
 # Iterate over each language passed as an argument
-for lang in "$@"; do
+for lang in "${@:2}"; do
     echo "Processing language: $lang"
     python edit.py --data_file datasets/v5/hard/$lang/translated/dataset.json \
-                   --hparam hparams/FT/llama-3-1-8b-hf.yaml \
+                   --hparam hparams/$method/llama-3-1-8b-hf.yaml \
                    --lang en \
                    --tgt_langs $lang \
-                   --log_subdir v5_hard_prompts_$lang \
+                   --log_subdir v5_hard_prompts_${method}_$lang \
                    --prompt_type prompts \
                    --tgt_prompt_type prompts prompts_gloss \
                    --device 0
