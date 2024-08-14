e14fac9e74eede50c1fb329d82f11c876eb04aab
Félix Gaschi <felix@posos.fr>
2024-08-14 15:33:53 +0000

    refactor(tables): improve tables

--- figures/hard_subset_tables.py
+++ figures/hard_subset_tables.py
@@ -1,36 +1,65 @@
 
 import json
 import argparse
+import itertools
 
 import pandas as pd
 
+def format_row_wise(styler, formatter):
+    for row, row_formatter in formatter.items():
+        row_num = styler.index.get_loc(row)
+
+        for col_num in range(len(styler.columns)):
+            styler._display_funcs[(row_num, col_num)] = row_formatter
+    return styler
 
 def create_table(langs: list[str], method: str):
-    line_names = ["accuracy", "X-portab. w/o gloss", "X-portab. w/ gloss"]
+    line_names = ["accuracy", "X-portab. w/o gloss", "X-portab. w/ gloss", "w/ - w/o"]
     getters = [
         lambda x, lang: x["rewrite_acc"],
         lambda x, lang: x["portability"][f"prompts-prompts_en-{lang}_acc"],
-        lambda x, lang: x["portability"][f"prompts-prompts_gloss_en-{lang}_acc"]
+        lambda x, lang: x["portability"][f"prompts-prompts_gloss_en-{lang}_acc"],
+        lambda x, lang: x["portability"][f"prompts-prompts_gloss_en-{lang}_acc"] - x["portability"][f"prompts-prompts_en-{lang}_acc"]
     ]
     data = {
-        "index": [*(("before", name) for name in line_names), *(("after", name) for name in line_names)],
+        "index": [("samples", ""), *(("before", name) for name in line_names), *(("after", name) for name in line_names)],
         "columns": langs,
         "index_names": ["", ""],
         "column_names": ["langs"],
-        "data": [[] for _ in range(len(line_names) * 2)]
+        "data": [[] for _ in range(len(line_names) * 2 + 1)]
     }
     for lang in langs:
         with open(f"logs/v5_hard_prompts_{method}_{lang}/summary.json", "r") as f:
             raw_result = json.load(f)
+        with open(f"logs/v5_hard_prompts_{method}_{lang}/results.json", "r") as f:
+            n_samples = len(json.load(f))
         
+        data["data"][0].append(n_samples)
         for i, getter in enumerate(getters):
-            data["data"][i].append(getter(raw_result["pre"], lang))
+            data["data"][i + 1].append(getter(raw_result["pre"], lang))
         for i, getter in enumerate(getters):
-            data["data"][i + len(getters)].append(getter(raw_result["post"], lang))
+            data["data"][i + 1 + len(getters)].append(getter(raw_result["post"], lang))
 
     df = pd.DataFrame.from_dict(data, orient="tight")
 
-    return df.style.format(formatter=lambda x: f"{100 * x:.1f}").to_latex(hrules=False, clines="skip-last;index")
+    class LogGetitemDict(dict):
+
+        def __getitem__(self, value):
+            print(f"retrieving key: {value}")
+            return super(LogGetitemDict, self).__getitem__(value)
+        
+        def __contains__(self, key: object) -> bool:
+            print(f"checking key: {key}")
+            return super().__contains__(key)
+
+    formatter = {
+        index: (lambda x: f"{int(x)}") if index == ("samples", "") else (lambda x: f"{100 * x:.1f}")
+        for index in data["index"]
+    }
+
+    print(df.index)
+
+    return format_row_wise(df.style, formatter).to_latex(hrules=False, clines="skip-last;index")
 
 
 if __name__ == "__main__":
