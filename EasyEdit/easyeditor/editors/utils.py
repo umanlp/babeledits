fixes to utils.py to address the sub_alias averaging problem

EasyEdit/easyeditor/editors/utils.py
@@ -43,27 +43,62 @@ def summary_metrics(all_metrics, eval_metrics, locality_metrics, rewrite_metrics
             else:
                 continue
             for metric_type in rewrite_metrics:
-                mean_metrics[eval][key].update({metric_type : float(np.mean([np.max(score[eval][key][metric_type]) for score in all_metrics]))})
+                mean_metrics[eval][key].update(
+                    {
+                        metric_type: float(
+                            np.mean(
+                                [
+                                    np.max(score[eval][key][metric_type])
+                                    for score in all_metrics
+                                ]
+                            )
+                        )
+                    }
+                )
         if "ppl" in all_metrics[0][eval].keys():
             mean_metrics[eval]["ppl"] = dict()
             for lang in all_metrics[0][eval]["ppl"]:
-                mean_metrics[eval]["ppl"][lang] = float(np.mean([score[eval]["ppl"][lang] for score in all_metrics]))
-        for key in ["rephrase_acc", "locality", "portability"]:
-                mean_metrics[eval][key] = dict()
-                if key in all_metrics[0][eval].keys() and all_metrics[0][eval][key] != {}:
-                    for prompt_type in all_metrics[0][eval][key]:
-                        mean_metrics[eval][key][prompt_type] = dict()
-                        metrics_to_gather = eval_metrics if key != "locality" else locality_metrics
-                        for metric_type in metrics_to_gather:
-                            mean_metrics[eval][key][prompt_type].update({metric_type: float(np.mean([np.max(score[eval][key][prompt_type][metric_type]) for score in all_metrics if prompt_type in score[eval][key]]))})
-
-                    # for lkey in get_all_acc_keys(all_metrics):
-                    #     metrics = [metric[eval][metric_type][key][lkey] for metric in all_metrics if lkey in metric[eval][metric_type][key].keys()]
-                    #     if len(metrics) > 0:
-                    #         mean_metrics[eval][metric_type][key][lkey] = np.mean(metrics)
-                        # mean_metrics[eval][key][lkey] = np.mean(
-                        #     [metric[eval][key][lkey] for metric in all_metrics])
-    # mean_metrics["time"] = np.mean([metric["time"] for metric in all_metrics])
+                mean_metrics[eval]["ppl"][lang] = float(
+                    np.mean([score[eval]["ppl"][lang] for score in all_metrics])
+                )
+    
+        def get_all_prompts(all_metrics, key):
+            l = [t[eval][key].keys() for t in all_metrics]
+            return sorted(list(set([item for sublist in l for item in sublist])))
+    
+        def get_all_other_keys(all_metrics, eval):
+            l = [t[eval].keys() for t in all_metrics]
+            output = list(
+                set(
+                    [
+                        item
+                        for sublist in l
+                        for item in sublist
+                        if item not in ["rewrite_acc", "rewrite_ppl", "ppl"]
+                    ]
+                )
+            )
+            return sorted(output)
+    
+        for key in get_all_other_keys(all_metrics, eval):
+            mean_metrics[eval][key] = dict()
+            for prompt_type in get_all_prompts(all_metrics, key):
+                mean_metrics[eval][key][prompt_type] = dict()
+                metrics_to_gather = eval_metrics if key != "locality" else locality_metrics
+                for metric_type in metrics_to_gather:
+                    mean_metrics[eval][key][prompt_type].update(
+                        {
+                            metric_type: float(
+                                np.mean(
+                                    [
+                                        np.max(score[eval][key][prompt_type][metric_type])
+                                        for score in all_metrics
+                                        if prompt_type in score[eval][key]
+                                    ]
+                                )
+                            )
+                        }
+                    )
 
     return mean_metrics
 