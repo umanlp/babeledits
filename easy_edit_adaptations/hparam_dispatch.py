5f1de30445c0d2f401d685a2e4e858d58a56879d
tommaso-green <tommaso.green@gmail.com>
2024-11-13 15:36:25 +0100

    r-rome hparams support

--- easy_edit_adaptations/hparam_dispatch.py
+++ easy_edit_adaptations/hparam_dispatch.py
@@ -1,15 +1,23 @@
 import sys
 sys.path.append('EasyEdit')
 
-from EasyEdit.easyeditor.models import ROMEHyperParams, MEMITHyperParams, IKEHyperParams, MENDHyperParams, FTHyperParams
+from EasyEdit.easyeditor.models import (
+    ROMEHyperParams,
+    MEMITHyperParams,
+    IKEHyperParams,
+    MENDHyperParams,
+    FTHyperParams,
+    R_ROMEHyperParams,
+)
 from EasyEdit.easyeditor.util import HyperParams
 
 methods = {
     "rome": ROMEHyperParams,
+    "r-rome": R_ROMEHyperParams,
     "memit": MEMITHyperParams,
     "ike": IKEHyperParams,
     "mend": MENDHyperParams,
-    "ft": FTHyperParams
+    "ft": FTHyperParams,
 }
 
 def get_hparm_class(method: str) -> type[HyperParams]:
