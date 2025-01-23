837dac0f7f1fde2445a4ec0305f4c7a17088bbea
tommaso-green <tommaso.green@gmail.com>
2025-01-23 22:23:22 +0100

    setting max number of new tokens to 50

--- eval.sh
+++ eval.sh
@@ -25,5 +25,6 @@ uv run python exec_eval.py \
   --tasks belebele_arb_Arab,belebele_deu_Latn,belebele_eng_Latn,belebele_fra_Latn,belebele_hrv_Latn,belebele_ita_Latn,belebele_jpn_Jpan,belebele_kat_Geor,belebele_mya_Mymr,belebele_zho_Hans,xquad_ar,xquad_de,xquad_en,xquad_zh \
   --batch_size auto \
   --device cuda:0 \
+  --gen_kwargs "max_new_tokens=50" \
   --output_path $EVAL_DIR \
   --load_weights $EVAL_DIR/edited_weights.pkl.gz
