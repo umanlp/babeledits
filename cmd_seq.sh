da2ebdc805bb95d20b29ea96975db819af43f201
tommaso-green <tommaso.green@gmail.com>
2025-01-27 23:04:55 +0100

    adding grace, changed times, generalized platform

--- cmd_seq.sh
+++ cmd_seq.sh
@@ -1,8 +1,8 @@
 
 declare -A dict=(
-    [100]=2
-    [250]=3
-    [500]=9
+    [100]=5
+    [250]=7
+    [500]=15
     [1042]=20
 )
 
@@ -12,7 +12,7 @@ while [[ $# -gt 0 ]]; do
     case $key in
         --method)
             METHOD="$2"
-            if [[ "$METHOD" != "ft-m" && "$METHOD" != "ft-l" && "$METHOD" != "r-rome" && "$METHOD" != "babelreft" ]]; then
+            if [[ "$METHOD" != "ft-m" && "$METHOD" != "ft-l" && "$METHOD" != "r-rome" && "$METHOD" != "babelreft"  && "$METHOD" != "grace"]]; then
                 echo "Invalid method: $METHOD"
                 exit 1
             fi
@@ -44,22 +44,28 @@ fi
 echo "Number of edits: $NUM_EDITS"
 echo "Max time: $MIN (minutes)"
 
+export PLATFORM="helix"
 if [[ "$METHOD" == "ft-m" ]]; then
     echo "Method: $METHOD"
-    uv run python edit.py -m hydra/launcher=helix_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=ft edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.objective_optimization=target_new method.layers=[21] method.lr=0.0005 
+    uv run python edit.py -m hydra/launcher=${PLATFORM}_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=ft edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.objective_optimization=target_new method.layers=[21] method.lr=0.0005 
 fi
 
 if [[ "$METHOD" == "ft-l" ]]; then
     echo "Method: $METHOD"
-    uv run python edit.py -m hydra/launcher=helix_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=ft edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.objective_optimization=prompt_last method.layers=[19] method.lr=0.0001 method.norm_constraint=0.002
+    uv run python edit.py -m hydra/launcher=${PLATFORM}_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=ft edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.objective_optimization=prompt_last method.layers=[19] method.lr=0.0001 method.norm_constraint=0.002
 fi
 
 if [[ "$METHOD" == "r-rome" ]]; then
     echo "Method: $METHOD"
-    uv run python edit.py -m hydra/launcher=helix_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=r-rome subject_in_prompt=loose edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.kl_factor=1 method.layers=[17]
+    uv run python edit.py -m hydra/launcher=${PLATFORM}_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=r-rome subject_in_prompt=loose edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.kl_factor=1 method.layers=[17]
 fi
 
 if [[ "$METHOD" == "babelreft" ]]; then
     echo "Method: $METHOD"
-    uv run python edit.py -m hydra/launcher=helix_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=babelreft subject_in_prompt=loose edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.layers=[12] method.low_rank_dim=64 method.lr=0.002
+    uv run python edit.py -m hydra/launcher=${PLATFORM}_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=babelreft subject_in_prompt=loose edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.layers=[12] method.low_rank_dim=64 method.lr=0.002
+fi
+
+if [[ "$METHOD" == "grace" ]]; then
+    echo "Method: $METHOD"
+    uv run python edit.py -m hydra/launcher=${PLATFORM}_min hydra.launcher.timeout_min=${MIN} log_subdir=v8_rev7_seq_${NUM_EDITS} max_edits=${NUM_EDITS} model=llama-3-1 method=grace edit_lang=en pre_edit=logs/v8_rev7/meta-llama_Meta-Llama-3.1-8B-Instruct/FT-M/en/prompts_mt_marked/ppl_test_set.json.gz sequential=true return_edited_weights_at_end=true method.edit_lr=0.1 method.eps=100 method.layers=[21] method.replacement=replace_all
 fi
